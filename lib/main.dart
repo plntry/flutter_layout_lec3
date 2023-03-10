ce3aafaeeb2caa7147bbdb31404410ed2c498d68
Oksana Nedilko <nedilko7b@gmail.com>
2023-03-10 17:04:52 +0200

    added dialog row template

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,65 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
-        body: const Text('body'),
+        body: ListTile(
+          contentPadding: const EdgeInsets.only(top: 10, left: 10),
+          leading: const CircleAvatar(
+            radius: 30,
+          ),
+          title: Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  'Arthur',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 19,
+                  ),
+                ),
+                Text(
+                  '12:56 PM',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'How are you?',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 6.5,
+                    vertical: 2,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Colors.grey,
+                  ),
+                  child: const Text(
+                    '5',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
       debugShowCheckedModeBanner: false,
     );
